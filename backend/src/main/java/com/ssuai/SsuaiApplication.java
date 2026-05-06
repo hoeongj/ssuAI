7f96f528ce27716d33b90ab96cb9e868736ca55b
hoengj <seongjuice999@gmail.com>
2026-05-06 14:08:24 +0900

    chore(backend): add missing application entrypoint and   profiles

---
+++ backend/src/main/java/com/ssuai/SsuaiApplication.java
@@ -0,0 +1,13 @@
+package com.ssuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsuaiApplication.class, args);
+    }
+}
+
